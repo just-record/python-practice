a01c3f375cbbbb61892ceb3b15c50511fdf68fec
jbsh <opmorn@gmail.com>
2024-01-25 18:54:55 +0900

    Add star drawing functionality with packages

---
+++ 01.drawing_stars/05.drawing_stars_with_packages/star_drawing/star_drawer.py
@@ -0,0 +1,9 @@
+class StarDrawer:
+    def __init__(self, alias='StarDrawer', num_lines=5):
+        self.alias = alias
+        self.num_lines = num_lines
+
+    def draw_stars(self):
+        print(f'{self.alias}이(가) 그립니다.')
+        for i in range(self.num_lines):
+            print((' ' * (self.num_lines - i)) + '*' * (i * 2 + 1))
